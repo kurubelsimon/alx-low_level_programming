d5296609905b3e5c551f1a73965c3e826e54d31d
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-14 16:38:11 -0700

    out put using printf in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry point 
+ * my hello ,printf task
+ * Return 1 after the program executed
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art.\n");
+	return (0);
+}
