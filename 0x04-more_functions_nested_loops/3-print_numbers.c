cf0df93e46a9ee642b071104555fe2cc0115ea46
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 15:56:51 -0700

    print 3 numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print numbers -print numbers
+*
+Return : always 0,
+[B[A[A[B[C[C[C[C[C[C[C[C[C[C[C[[A[CReturn : always 0.
+*/
+void print_numbers(void)
+{
+int i;
+i=0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
