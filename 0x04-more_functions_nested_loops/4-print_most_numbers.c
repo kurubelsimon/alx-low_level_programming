c9e9b25d95ec0a4edd863cfa8b8420fced48d51e
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 15:59:59 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main - Check the code 
+*
+* Return : always 0/
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[.[1~
+*/
+int main(void)
+{
+	print_most_numbers()l
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;
+	return (0);
+}
