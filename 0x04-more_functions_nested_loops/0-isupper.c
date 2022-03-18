a34531e4105e449d9c2e5736d8739aabc6c2b0ea
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 15:00:30 -0700

    third edition  on is upper or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
 *main -check the code.
 *
-*Return : always 0.
+*Return : Always 0.
 */
-int main (void)
+int main(void)
 {
 char c;
 c='A';
-printf("%c: %d\n" , c, _isupper(c));
-c='a'
-printf("%c: %d\n" , c, _isupper(c));
+puts("%c: %d\n" , c, _isupper(c));
+c='a';
+puts("%c: %d\n" , c, _isupper(c));
 return (0);
 }
