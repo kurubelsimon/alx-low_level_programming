aa0f23540d37e61f5934d41486b43d5423c089a8
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 16:21:54 -0700

    print 6 lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main -check the code
+*Return : always 0
+*/
+int main(void)
+{
+_putchar(0);
+_putchar(1);
+_putchar(2);
+[A[C[C[C[C[C[C[C[C[[[C[([[C[C[C[Cr(10);[A[D[D[2[C[C[B
+_putchar(-4);
+return (0);
+}
