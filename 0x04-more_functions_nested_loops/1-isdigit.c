a914deb8954da3cabe3e86d1e22bfd4e6328dbe3
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 14:46:41 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isdigit - if is a digit
+*@c:print int
+*Return: Always 0.
+*/
+int _isdigit(int c)
+{
+if (c >=48 && c <= 57)
+{
+return (1);
+}
+else{
+[A[C[C[C[C[
+{
+return (0);
+}
+}
