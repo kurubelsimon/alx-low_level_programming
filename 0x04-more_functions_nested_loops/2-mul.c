ac98cacad91e39286427595f57f84237cb9d1f69
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-18 15:20:16 -0700

    this is thrid task of multiply two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code.
- *
- * Return : always 0.
- */
-int main(void)
+*mul -multiplies two numbers
+*@a: print int
+*@b: print int 
+* Return : Always 0.
+*/
+int mul (int a, int b)
 {
-	printf("%d\n" , mul(98, 1024));
-	printf("%d\n" , mul(-402, 4096));
-	return (0);
+return (a * b);
 }
