e20730cd664fb8fdac5369c81c8b6ba9fa6f52e7
kurubelsimon <kurubelsimon19@gmail.com>
2022-03-22 14:54:32 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,6 @@
 *
 * return : always 0.
 */
-[A[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C*n : a pointer to n int that will be updated[A
-[C[C[D[D*[C[C[Cn[D[D*[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[A[A[A[C[C[B[C reset to 98 -update a integer value to 98[B[B[B[B[B
 void reset_to_98(int *n){
-*n=98;
-}
+	*n=98;
+};
